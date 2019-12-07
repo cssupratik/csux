62efdc4783c9ecdde60bc877f7261f72e45e5eeb
cssupratik <cs.supratik@zoho.com>
2019-12-07 08:06:56 -0800

    index html using local bootstrap.css from node_modules

--- ccui_module/webpack.config.js
+++ ccui_module/webpack.config.js
@@ -1,16 +1,29 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: './src/main.ts',
     resolve: {
         extensions: ['.ts', '.js']
     },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+      },
     module: {
         rules: [
             {
                 test: /\.ts$/,
                 use: ['ts-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: 'sass-loader' },
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]  
             },
             {
                 test: /\.(html|css)$/,
@@ -31,7 +44,7 @@ module.exports = {
         new webpack.DefinePlugin({
             // global app config object
             config: JSON.stringify({
-                apiUrl: 'http://10.230.163.34:4000'
+                apiUrl: 'http://localhost:4000'
             })
         })
     ],
